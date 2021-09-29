b00e4ec5be44fdb770eab7e8aca06ea55a924031
Liel <liel.izcha321@gmail.com>
2021-09-30 00:07:20 +0300

    first commit

---
+++ 10.08.2021/תרגול עצמאי/index.js
@@ -0,0 +1,14 @@
+//1...................................................
+// function revearsArray(theArray) {
+//   theArray.reverse();
+//   return theArray;
+// }
+// console.log(revearsArray([1, 2, 3, 4]));
+
+function multiply(a, b){
+    a * b
+  }
+  multiply(4,2)
+  
+  
+  console.log(multiply(4,2));
