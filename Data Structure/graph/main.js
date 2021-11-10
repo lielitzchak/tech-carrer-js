e9fb342ca65a2ff9dbc0aff7b60c340956f40603
Liel Itzchak <liel.izcha321@gmail.com>
2021-11-10 13:42:41 +0200

    10.11.2021

---
+++ Data Structure/graph/main.js
@@ -0,0 +1,27 @@
+//! throw Error =to learn that ;
+class Graph {
+  constructor() {
+    this.contactWith = {};
+  }
+  addNewVertex(key) {
+    if (!this.contactWith[key]) {
+      this.contactWith[key] = [];
+    } else {
+      console.log("error");
+    }
+  }
+  
+  connectVertex(vertex1, vertex2) {
+    if (this.contactWith[vertex1]) {
+      this.contactWith[vertex1].push(vertex2);
+      this.contactWith[vertex2].push(vertex1);
+    }
+    console.log(vertex1, vertex2);
+  }
+}
+
+let x = new Graph();
+x.addNewVertex("a");
+x.addNewVertex("b");
+x.connectVertex("a", "b");
+console.log(x.contactWith);
