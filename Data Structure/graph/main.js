da74541d39f0e6e0cd7c910d0271e9598e8cde84
Liel Itzchak <liel.izcha321@gmail.com>
2021-11-10 16:56:51 +0200

    10.11.2021

--- Data Structure/graph/main.js
+++ Data Structure/graph/main.js
@@ -10,18 +10,23 @@ class Graph {
       console.log("error");
     }
   }
-  
+
   connectVertex(vertex1, vertex2) {
+    let checkIfExist = this.contactWith[vertex1] && this.contactWith[vertex2];
     if (this.contactWith[vertex1]) {
       this.contactWith[vertex1].push(vertex2);
       this.contactWith[vertex2].push(vertex1);
     }
-    console.log(vertex1, vertex2);
+  }
+  remove(vertex1) {
+    delete this.contactWith[vertex1];
   }
 }
 
 let x = new Graph();
 x.addNewVertex("a");
 x.addNewVertex("b");
+x.addNewVertex("c");
 x.connectVertex("a", "b");
+// x.remove("a");
 console.log(x.contactWith);
