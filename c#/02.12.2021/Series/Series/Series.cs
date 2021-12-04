b59887f231f2f730e677311493b07bb4589df9e3
Liel Itzchak <liel.izcha321@gmail.com>
2021-12-04 20:43:14 +0200

    04.12.2021

---
+++ c#/02.12.2021/Series/Series/Series.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Series
+{
+    public class SeriesCalss : IComparable
+    {
+        private string name;
+        private string genre;
+        private int episodes;
+        public SeriesCalss(string _name, string _genre, int _episodes)
+        {
+            name = _name;
+            genre = _genre;
+            episodes = _episodes;
+
+        }
+
+        public int CompareTo(object? obj)
+        {
+            SeriesCalss ser = (SeriesCalss)obj;
+            if (ser.episodes > this.episodes)
+            {
+                return 1;
+            }
+            if (ser.episodes < this.episodes)
+            {
+                return -1;
+            }
+            return 0;
+
+        }
+      public  void addMove() 
+        {
+
+        }
+    }
+}
