aa1d092f0fdd3bfea3eb1b17c8d06797317362e8
Liel Itzchak <liel.izcha321@gmail.com>
2021-12-06 00:05:22 +0200

    06.12.2021

---
+++ אנסיכרוניות/25.10.2021/users.js
@@ -0,0 +1,364 @@
+const USERS = JSON.stringify([
+  {
+    _id: "605acace4ab389d8ed54c496",
+    age: 35,
+    name: {
+      last: "Hensley",
+      first: "Dollie",
+    },
+    email: "dollie.hensley@undefined.org",
+    index: 0,
+    phone: "+1 (904) 421-3160",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acaceed1f0e1cfa1eee67",
+    age: 21,
+    name: {
+      last: "Hunt",
+      first: "Dolores",
+    },
+    email: "dolores.hunt@undefined.com",
+    index: 1,
+    phone: "+1 (967) 432-2905",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acacefedd0fe77874d78f",
+    age: 28,
+    name: {
+      last: "Pennington",
+      first: "Milagros",
+    },
+    email: "milagros.pennington@undefined.net",
+    index: 2,
+    phone: "+1 (922) 492-2085",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acace6d3f84fc9b40849f",
+    age: 32,
+    name: {
+      last: "Anderson",
+      first: "Myrtle",
+    },
+    email: "myrtle.anderson@undefined.biz",
+    index: 3,
+    phone: "+1 (858) 404-3273",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acace06d24518b6419d23",
+    age: 29,
+    name: {
+      last: "York",
+      first: "Terry",
+    },
+    email: "terry.york@undefined.co.uk",
+    index: 4,
+    phone: "+1 (816) 492-3475",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acace46f0669738455d6b",
+    age: 38,
+    name: {
+      last: "Bridges",
+      first: "Effie",
+    },
+    email: "effie.bridges@undefined.info",
+    index: 5,
+    phone: "+1 (856) 423-3786",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acace5ab5217354268532",
+    age: 26,
+    name: {
+      last: "Walter",
+      first: "Krista",
+    },
+    email: "krista.walter@undefined.me",
+    index: 6,
+    phone: "+1 (849) 513-2888",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acaced68677a5dca08a9b",
+    age: 20,
+    name: {
+      last: "Buchanan",
+      first: "Gomez",
+    },
+    email: "gomez.buchanan@undefined.biz",
+    index: 7,
+    phone: "+1 (888) 494-2462",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acaceb7ece55cb38e546a",
+    age: 20,
+    name: {
+      last: "Potter",
+      first: "liel",
+    },
+    email: "caitlin.potter@undefined.io",
+    index: 8,
+    phone: "+1 (801) 514-3055",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "605acace98de45687c4c6279",
+    age: 26,
+    name: {
+      last: "Dillon",
+      first: "Guzman",
+    },
+    email: "guzman.dillon@undefined.ca",
+    index: 9,
+    phone: "+1 (857) 407-2403",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+  {
+    _id: "31605c6eb7ebc255946a9197",
+    age: 21,
+    name: {
+      last: "itzchak",
+      first: "liel",
+    },
+    email: "liel.itzchak@undefined.ca",
+    index: 10,
+    phone: "54080093",
+    picture:
+      "https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80",
+  },
+]);
+let notJSON = JSON.parse(USERS);
+
+// ! exeresie 1
+
+function checkArrayEmpty() {
+  return new Promise((resolve, reject) => {
+    notJSON.length > 0 ? resolve(notJSON) : reject({ massage: "error" });
+  });
+}
+async function getCheckArrayEmpty() {
+  try {
+    return await checkArrayEmpty();
+  } catch (error) {
+    return error;
+  }
+}
+// getCheckArrayEmpty()
+//   .then((res) => {
+// console.log(
+//     res;
+// );
+//   })
+//   .catch((rej) => {
+//     console.log(rej);
+//   })
+//   .finally(() => {});
+
+// ! exeresie 2
+
+function checkId(id) {
+  return new Promise((resolve, reject) => {
+    for (let item of notJSON) {
+      if (item._id == id) {
+        resolve(item);
+      }
+    }
+    reject({ massage: "the user not found with your id number" });
+  });
+}
+async function getCheckId(id) {
+  try {
+    return await checkId(id);
+  } catch (error) {
+    return error;
+  }
+}
+// getCheckId("605acacefedd0fe77874d78f")
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((rej) => {
+//     console.log(rej);
+//   })
+//   .finally(() => {});
+// ! exeresie 3
+function checkEmail(email) {
+  return new Promise((resolve, reject) => {
+    for (let user of notJSON) {
+      if (user.email == email) {
+        resolve(user);
+      }
+    }
+    reject({ massage: "the user not found with your email" });
+  });
+}
+async function getCheckEmail(id) {
+  try {
+    return await checkEmail(id);
+  } catch (error) {
+    return error;
+  }
+}
+// getCheckEmail("effie.bridges@undefined.info")
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((rej) => {
+//     console.log(rej);
+//   })
+//   .finally(() => {});
+// ! exeresie 4
+
+function CheckAge(num) {
+  let userAge = [];
+  return new Promise((resolve, reject) => {
+    for (let user of notJSON) {
+      if (user.age > num) {
+        userAge.push(user);
+      }
+    }
+    if (userAge.length > 0) {
+      resolve(userAge);
+    } else {
+      reject({ massage: "the user not found with your age number" });
+    }
+  });
+}
+async function getCheckAge(age) {
+  try {
+    return await CheckAge(age);
+  } catch (error) {
+    return error;
+  }
+}
+// getCheckAge(21)
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((rej) => {
+//     console.log(rej);
+//   })
+//   .finally(() => {});
+
+// ! exeresie 5
+function CheckFirstName(firstName) {
+  return new Promise((resolve, reject) => {
+    for (let user of notJSON) {
+      if (user.name.first == firstName) {
+        resolve(user);
+      }
+    }
+    reject({ massage: "the user not found with your first name" });
+  });
+}
+async function getCheckFirstName(name) {
+  try {
+    return await CheckFirstName(name);
+  } catch (error) {
+    return error;
+  }
+}
+//! exeresie 5 a
+function CheckLastName(lastName) {
+  return new Promise((resolve, reject) => {
+    for (let user of notJSON) {
+      if (user.name.last == lastName) {
+        resolve(user);
+      }
+    }
+    reject({ massage: "the user not found with your last name" });
+  });
+}
+async function getCheckLastName(name) {
+  try {
+    return await CheckLastName(name);
+  } catch (error) {
+    return error;
+  }
+}
+
+// getCheckIsUserName()
+// .then((res) => {
+//   console.log(res);
+// })
+// .catch((rej) => {
+//   console.log(rej);
+// })
+// .finally(() => {});
+
+// ! exeresie 6
+let formFrom = document.getElementById("formFromUser");
+let printTble = document.getElementById("printTble");
+let selectOption = document.getElementById("selectOption");
+let printToWindow = document.getElementById("printToWindow");
+let spinner = document.getElementById("spinner");
+formFrom.onclick = (ev) => {
+  ev.preventDefault();
+  spinner.classList.remove("hidden");
+  clear();
+  showImg();
+  setTimeout(() => {
+    switch (selectOption.value) {
+      case "id":
+        let userPromise = checkId(textFromUser.value);
+        userPromise
+          .then((user) => {
+            display(user);
+          })
+          .catch(() => {
+            printToWindow.innerHTML = "error";
+          });
+        break;
+      case "age":
+        let promisAge = getCheckAge(textFromUser.value);
+        promisAge
+          .then((res) => {
+            display(res);
+          })
+          .catch(() => {
+            printToWindow.innerHTML = "error";
+          })
+          .finally(() => {});
+        break;
+      case "firstName":
+        let nam = CheckFirstName(textFromUser.value);
+        nam
+          .then((res) => {
+            display(res);
+          })
+          .catch(() => {});
+        console.log("first name");
+    }
+    spinner.classList.add("hidden");
+  }, 2000);
+};
+function display(user) {
+  printToWindow.innerText += user.name.first;
+  printToWindow.innerHTML += `<br>`;
+  printToWindow.innerText += user.name.last;
+  printToWindow.innerHTML += `<br>`;
+  printToWindow.innerText += user.email;
+  printToWindow.innerHTML += `<br>`;
+  printToWindow.innerHTML += `<img src=" ${user.picture}">
+ `;
+}
+function clear() {
+  printToWindow.innerHTML = "";
+}
+function showImg() {}
---
+++ אנסיכרוניות/movies.js
@@ -0,0 +1,196 @@
+let movies = JSON.stringify({
+  success: true,
+  data: [
+    {
+      _id: "606d7d2c110a540674caa27a",
+      movieName: "The Starling",
+      rating: 3,
+      date: "2021-04-07T09:36:44.053Z",
+      __v: 0,
+    },
+    {
+      _id: "607828bcd9def60015535fcc",
+      movieName: "All the Bright Places",
+      rating: 3,
+      date: "2021-04-15T11:51:24.833Z",
+      __v: 0,
+    },
+    {
+      _id: "60815daa6b305d8dcd07c674",
+      movieName: "Guilty",
+      rating: 3,
+      date: "2021-04-22T11:27:38.067Z",
+      __v: 0,
+    },
+    {
+      _id: "6122459bdbf6109484b6d2eb",
+      movieName: "He's All That",
+      rating: 4,
+      date: "2021-08-22T12:39:55.115Z",
+      __v: 0,
+    },
+    {
+      _id: "612245fcbb593194fcda74d0",
+      movieName: "The Forgotten Battle",
+      rating: 3,
+      date: "2021-08-22T12:41:32.969Z",
+      __v: 0,
+    },
+  ],
+});
+
+// ! exeresie 1
+
+function returnJs() {
+  return new Promise((resolve, reject) => {
+    JSON.parse(movies).success == true
+      ? resolve(JSON.parse(movies))
+      : reject("error");
+  });
+}
+
+async function getReturnJs() {
+  try {
+    return await returnJs();
+  } catch (error) {
+    return error;
+  }
+}
+getReturnJs()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((rej) => {
+    console.log(rej);
+  })
+  .finally(() => {});
+// ! exeresie 2
+function checkIfArrayExist() {
+  return new Promise((resolve, reject) => {
+    let temp = JSON.parse(movies).data;
+    if (temp.length > 0) {
+      resolve(temp);
+    } else {
+      reject("error");
+    }
+  });
+}
+async function gatLielFUNCTION() {
+  try {
+    return await checkIfArrayExist();
+  } catch (error) {
+    return { massage: error };
+  }
+}
+gatLielFUNCTION()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((rej) => {
+    console.log(rej);
+  })
+  .finally(() => {});
+// ! exeresie 3
+function searchMovieByName(nameMovie) {
+  let arrayMovies = JSON.parse(movies).data;
+  return new Promise((resolve, reject) => {
+    for (let movie of arrayMovies) {
+      if (movie.movieName == nameMovie) {
+        resolve(movie);
+      }
+    }
+    reject({ massage: "error" });
+  });
+}
+async function getSearchMovieName(nameFile) {
+  try {
+    return await searchMovieByName(nameFile);
+  } catch (error) {
+    return console.log(error);
+  }
+}
+getSearchMovieName("Guilty")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((rej) => {
+    console.log(rej);
+  })
+  .finally(() => {});
+// ! exeresie 4
+
+function searchMovieById(idMovie) {
+  let moviesData = JSON.parse(movies).data;
+  return new Promise((resolve, reject) => {
+    for (let item of moviesData) {
+      if (item._id === idMovie) {
+        resolve("good");
+      }
+    }
+    reject("error");
+  });
+}
+async function getSearchMovieById(file) {
+  try {
+    return await searchMovieById(file);
+  } catch (error) {
+    return { massage: error };
+  }
+}
+getSearchMovieById("60815daab305d8dcd07c674")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((rej) => {
+    console.log(rej);
+  })
+  .finally(() => {});
+
+// ! exeresie 5
+function searchMovieByRating(numFromUser) {
+  let moviesData = JSON.parse(movies).data;
+  let moviesByRating = [];
+  console.log(moviesData);
+  return new Promise((resolve, reject) => {
+    for (let item of moviesData) {
+      if (item.rating > numFromUser) {
+        moviesByRating.push(item);
+        resolve(moviesByRating);
+      }
+    }
+    reject({ massage: "error" });
+  });
+}
+async function getSearchMovieByRating(num) {
+  try {
+    return await searchMovieByRating(num);
+  } catch (error) {
+    return error;
+  }
+}
+getSearchMovieByRating(3)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((rej) => {
+    console.log(rej);
+  })
+  .finally(() => {});
+// ! exeresie 6
+
+let myForm = document.getElementById("myForm");
+let printMovies = document.getElementById("printMovies");
+let allMOvise = JSON.parse(movies).data;
+console.log(allMOvise);
+
+for (const move of allMOvise) {
+  printMovies.innerHTML += ` 
+    <tr>
+        <td>${move._id}<td>
+        <td>${move.movieName} <td>
+        <td>${move.rating} <td>
+        <td>${move.date} <td>
+        <td>${move.__v} <tr>
+    </tr>
+`;
+}
