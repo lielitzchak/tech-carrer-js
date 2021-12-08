39ce0c6ff4304b0d0f41cd8d23d571af0b6eb05e
Liel Itzchak <liel.izcha321@gmail.com>
2021-12-08 17:42:14 +0200

    08.12.2021

---
+++ c#/clinic/clinic/clinic/Doctor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clinic
+{
+    class Doctor : Employee, IComparable
+    {
+        private int numpatients;
+        private int birth;
+        public Doctor(string _FName, string _LName, string _Job, int _patients, int _birth) : base(_FName, _LName, _Job)
+        {
+            this.numpatients = _patients;
+            this.birth = _birth;
+        }
+        public int Numpatients
+        {
+            get { return this.numpatients; }
+            set { this.numpatients = value; }
+        }
+        public int Birth
+        {
+            get { return this.birth; }
+            set { this.birth = value; }
+        }
+        public int CompareTo(object obj)
+        {
+            Doctor doctor = (Doctor)obj;
+            if (this.numpatients < doctor.numpatients)
+            { return -1; }
+            if (this.numpatients < doctor.numpatients)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
+
