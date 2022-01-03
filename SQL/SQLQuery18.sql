760686de9a6a220bab7f7e406b45ae12d00b464e
liel itzchak <liel.izcha321@gmail.com>
2022-01-03 08:35:28 +0200

    sql

---
+++ SQL/SQLQuery18.sql
@@ -0,0 +1,47 @@
+ --1. 
+ --SELECT *
+ -- FROM Employees
+
+ --2
+
+ --SELECT * FROM Employees 
+ --WHERE BirthDate<'01-01/1990'
+
+
+ --3
+ --SELECT LastName ,FirstName ,Title FROM Employees
+	--where City ='London'
+ 
+ -- 4
+
+ --SELECT * FROM Products
+ --WHERE UnitPrice<20
+ 
+
+ --5
+
+--SELECT* FROM Products	
+--ORDER BY UnitsInStock
+
+--6
+
+--SELECT * FROM Customers
+--ORDER BY City
+
+
+
+--7
+
+--SELECT * FROM Customers
+--  WHERE CompanyName LIKE 'E%'
+
+
+--8
+
+
+
+
+--9
+--INSERT INTO Employees(EmployeeID, LastName , FirstName , TitleOfCourtesy ,BirthDate , HireDate ,Address ,City,Region ,PostalCode,Country,HomePhone,Extension,Photo ,Notes ,Region ,PhotoPath)
+--VALUES ('ITZCHAK','LIEL','IDK','MR.',30/03/2000,12/06/2021,NULL, 'LONDON' ,NULL, 56320 ,'israel',9978784,4429,'HELLOWORLDPHOTO' ,'form YOU NOTE' , 5 , 'LINKtOsEE')
+
