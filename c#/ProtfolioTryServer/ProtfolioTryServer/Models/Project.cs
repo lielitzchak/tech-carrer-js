c28867d173f5f9ae21a1321dd84591e5199a29d9
liel itzchak <liel.izcha321@gmail.com>
2022-01-13 23:43:20 +0200

    cs

---
+++ c#/ProtfolioTryServer/ProtfolioTryServer/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProtfolioTryServer.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string AppName { get; set; }
+        public string Description { get; set; }
+        public string imag { get; set; }
+        public string techWorkWith { get; set; }
+        public string LinkToGit { get; set; }
+    }
+}
