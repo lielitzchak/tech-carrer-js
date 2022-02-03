cfea51d9eae83b8a5cc39ee3fb52f53f38a2f7b5
liel itzchak <liel.izcha321@gmail.com>
2022-02-03 09:27:27 +0200

    03.02.2021

---
+++ react/react with jacob/01.12.2021/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Foods from "./Component/Cars/Foods";
+import Employees from './Component/employees/Employees'
+
+function App() {
+  return (
+    <div className="App">
+      <Employees/>
+      <Foods/>
+    </div>
+  );
+}
+
+export default App;
